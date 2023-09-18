4285a6b70a192c82432f4697b929062187088164
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2023-09-18 09:47:12 +0200

    v3

---
+++ 2tm5_1/powtorzenie.py
@@ -0,0 +1,27 @@
+''' Instrukcja warunkowa'''
+
+# liczba1 = float(input('Podaj liczbe pierwszą'))
+# liczba2 = float(input('Podaj liczbe drugą'))
+
+# if liczba1 > liczba2:
+#     print(f'Liczba {liczba1} jest większa od {liczba2}')
+# elif liczba1 < liczba2:
+#     print(f'Liczba {liczba2} jest większa od {liczba1}')
+# else:
+#     print('Podane liczby są identyczne')
+
+''' Instrukcja pętli'''
+
+# for i in range(11,5,-2):
+#     print(i)
+''' Lista'''
+lista = [1,2,3,4,'a']
+#
+
+for el in lista:
+    print(el)
+print('-------------')
+for i in range(2,len(lista),2):
+     print(lista[i])
+
+
