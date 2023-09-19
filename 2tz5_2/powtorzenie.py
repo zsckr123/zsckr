0dd349ae6f625ab544b32f5e6f47fa478fc25158
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2023-09-19 08:30:19 +0200

    v5

--- 2tz5_2/powtorzenie.py
+++ 2tz5_2/powtorzenie.py
@@ -2,7 +2,7 @@
 #
 # liczba1 = float(input('Podaj liczbe pierwszą'))
 # liczba2 = float(input('Podaj liczbe drugą'))
-# 
+#
 # if liczba1 > liczba2:
 #     print(f'Liczba {liczba1} jest większa od {liczba2}')
 # elif liczba1 < liczba2:
@@ -12,10 +12,13 @@
 
 ''' Instrukcja pętli'''
 
-# for i in range(10,2,-2):
+# for i in range(10,3,-1):
 #     print(i)
 ''' Lista'''
-# lista = [1,2,3,4,'a']
+lista = [1,2,3,4,'a','b','c']
+# print(lista[2::-2])
+# for el in lista:
+#     print(el)
 #
-# for i in range(0,len(lista),2):
-#      print(lista[i])
+for i in range(0,len(lista),2):
+     print(lista[i])
