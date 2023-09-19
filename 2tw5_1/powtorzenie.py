0afd1ff78d5cc6ecfcad618671707957701cbebf
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2023-09-19 10:55:41 +0200

    v5

--- 2tw5_1/powtorzenie.py
+++ 2tw5_1/powtorzenie.py
@@ -2,7 +2,7 @@
 #
 # liczba1 = int(input('Podaj liczbe pierwszą'))
 # liczba2 = int(input('Podaj liczbe drugą'))
-# #
+# # #
 # if liczba1 > liczba2:
 #     print(f'Liczba {liczba1} jest większa od {liczba2}')
 # elif liczba1 < liczba2:
@@ -15,14 +15,15 @@
 # for i in range(10,3,-2):
 #     print(i)
 # print('-----------------')
-# for i in range(1,10):
+# for i in range(3,10):
 #     print(i)
 ''' Lista'''
 lista = [1,2,3,4,'a','b','c']
-print(lista[2::2])
+print(lista[1:4])
+print(lista[4::-2])
 for el in lista:
     print(el)
-print('-------------')
-#
+# print('-------------')
+# #
 for i in range(0,len(lista),2):
      print(lista[i])
