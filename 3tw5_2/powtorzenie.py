5d4114be8a055ebe1242800bf5b89df62cfc79b5
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2023-09-19 10:18:16 +0200

    v5

--- 3tw5_2/powtorzenie.py
+++ 3tw5_2/powtorzenie.py
@@ -1,8 +1,8 @@
 ''' Instrukcja warunkowa'''
 #
-# liczba1 = float(input('Podaj liczbe pierwszą'))
-# liczba2 = float(input('Podaj liczbe drugą'))
-#
+# liczba1 = int(input('Podaj liczbe pierwszą'))
+# liczba2 = int(input('Podaj liczbe drugą'))
+# #
 # if liczba1 > liczba2:
 #     print(f'Liczba {liczba1} jest większa od {liczba2}')
 # elif liczba1 < liczba2:
@@ -12,13 +12,17 @@
 
 ''' Instrukcja pętli'''
 
-# for i in range(10,3,-1):
+# for i in range(10,3,-2):
+#     print(i)
+# print('-----------------')
+# for i in range(1,10):
 #     print(i)
 ''' Lista'''
 lista = [1,2,3,4,'a','b','c']
-# print(lista[2::-2])
-# for el in lista:
-#     print(el)
+print(lista[2::2])
+for el in lista:
+    print(el)
+print('-------------')
 #
 for i in range(0,len(lista),2):
      print(lista[i])
