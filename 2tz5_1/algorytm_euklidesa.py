cd1e522dc05c56fd3842201b004573142e42a7b7
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2023-09-25 08:27:46 +0200

    v6

---
+++ 2tz5_1/algorytm_euklidesa.py
@@ -0,0 +1,17 @@
+''' Dzielimy liczba1 przez liczba2 '''
+liczba1 = int(input('podaj pierwszą liczbę: '))
+liczba2 = int(input('podaj drugą liczbę: '))
+licznik = 0
+if liczba2 == 0:
+      print("NWD to:",liczba1)
+else:
+    while liczba2 > 0:
+        # licznik += 1
+        reszta = liczba1 % liczba2
+        liczba1 = liczba2
+        liczba2 = reszta
+
+# koniec pętli
+
+# print("pętla wykonana {} razy".format(licznik))
+print("najwiekszy wspolny dzielnik to:",liczba1)
--- 3rz5/powtorzenie.py
+++ 3rz5/powtorzenie.py
@@ -3,14 +3,14 @@
 
 # #
 
-# liczba1 = int(input('Podaj liczbe pierwszą'))
-# liczba2 = int(input('Podaj liczbe drugą'))
-# if liczba1 > liczba2:
-#         print(f'Liczba {liczba1} jest większa od {liczba2}')
-# elif liczba1 < liczba2:
-#         print(f'Liczba {liczba2} jest większa od {liczba1}')
-# else:
-#         print('Podane liczby są identyczne')
+liczba1 = int(input('Podaj liczbe pierwszą'))
+liczba2 = int(input('Podaj liczbe drugą'))
+if liczba1 > liczba2:
+        print(f'Liczba {liczba1} jest większa od {liczba2}')
+elif liczba1 < liczba2:
+        print(f'Liczba {liczba2} jest większa od {liczba1}')
+else:
+        print('Podane liczby są identyczne')
 
 ''' Pętla while'''
 # while True:
@@ -40,14 +40,14 @@
 # for i in range(10,3,-2):
 #     print(i)
 ''' Lista'''
-lista = [1,2,3,4,'a','b','c']
-print(lista)
-print(lista[1:4])
-print(lista[5:2:-2])
-
-for el in lista:
-    print(el)
-print('-------------')
-# # # #
-for i in range(0,len(lista),2):
-     print(lista[i])
+# lista = [1,2,3,4,'a','b','c']
+# print(lista)
+# print(lista[1:4])
+# print(lista[5:2:-2])
+#
+# for el in lista:
+#     print(el)
+# print('-------------')
+# # # # #
+# for i in range(0,len(lista),2):
+#      print(lista[i])
