d723f620d380d74bcc8d84d60f73152e9ab00d93
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2023-09-27 10:05:01 +0200

    v7

---
+++ 3rz5/systemy_liczbowe.py
@@ -0,0 +1,11 @@
+''' Zamiana liczby z systemu "10" na inny'''
+liczba = int(input('Podaj liczbę'))
+podstawa = int(input('Podaj podstawę'))
+lista = []
+while liczba > 0:
+    reszta = liczba % podstawa
+    wynik = liczba // podstawa
+    lista.append(reszta)
+    liczba = wynik
+
+print(lista[::-1])
