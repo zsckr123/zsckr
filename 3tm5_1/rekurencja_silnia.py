6baed68e86123f0a7d1a7da0ca18c037b45813bd
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2023-10-19 09:18:37 +0200

    v9

---
+++ 3tm5_1/rekurencja_silnia.py
@@ -0,0 +1,11 @@
+def silnia(n):
+    if n > 1:
+        return n * silnia(n-1)
+    return 1
+print(silnia(990))
+
+''' silnia(5) = 5 * silnia(4)
+ale silnia(4) = 4 * silnia(3)
+ale silnia(3) = 3 * silnia(2)
+ale silnia(2) = 2 * silnia(1)
+zatem zbieramy silnia(1) = 1'''
