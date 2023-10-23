eab38429b410b98ccdf19425bff917be17f69610
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2023-10-23 09:59:45 +0200

    v10

--- 2tm5_1/sortowanie_przez_wstawianie.py
+++ 2tm5_1/sortowanie_przez_wstawianie.py
@@ -2,7 +2,7 @@ def sort_insert(A):
     for i in range(1,len(A)):
         klucz = A[i]
         j = i - 1
-        while j >= 0 and A[j] < klucz: # zmiana znaku = zmiana typu sortowania
+        while j >= 0 and A[j] > klucz: # zmiana znaku = zmiana typu sortowania
             A[j + 1] = A[j]
             j = j - 1
         A[j + 1] = klucz
