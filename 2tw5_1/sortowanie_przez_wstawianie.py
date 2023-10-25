27af134ed17531a0565b73913f633cf840760b0d
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2023-10-25 09:07:55 +0200

    v11

---
+++ 2tw5_1/sortowanie_przez_wstawianie.py
@@ -0,0 +1,16 @@
+
+def sort_insert(A):
+
+    for i in range(1,len(A)):
+        klucz = A[i]
+        j = i - 1
+        while j >= 0 and A[j] > klucz: # zmiana znaku = zmiana typu sortowania
+            A[j + 1] = A[j]
+            j = j - 1
+        A[j + 1] = klucz
+
+    return A
+
+lista = [-2,3,4,5,-100, 200, -10000]
+# lista1 = [x for x in range(1,1000)]
+print(sort_insert(lista))
