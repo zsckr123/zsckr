ec1444ad6a4976370742b1130f0420cd5e071af4
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2023-11-02 08:33:36 +0100

    v12

---
+++ 3tw5_2/rekurencja_2.py
@@ -0,0 +1,13 @@
+''' a(1)=1
+ a(n+1) = a(n) +n
+ a(2) =2
+ a(3) = a(2) +2'''
+
+def ciag(n):
+    # if n == 1:
+    #     return 1
+    if n > 1:
+        return ciag(n-1) + (n-1)
+    return 1
+
+print(ciag(3))
--- 3tw5_2/rekurencja_silnia.py
+++ 3tw5_2/rekurencja_silnia.py
@@ -1,11 +1,14 @@
 def silnia(n):
+    # if n == 1:
+    #     return 1
     if n > 1:
-        return n * silnia(n-1)
+        return silnia(n-1) * n
     return 1
-print(silnia(900))
 
 ''' silnia(5) = 5 * silnia(4)
 ale silnia(4) = 4 * silnia(3)
 ale silnia(3) = 3 * silnia(2)
 ale silnia(2) = 2 * silnia(1)
 zatem zbieramy silnia(1) = 1'''
+
+print(silnia(5))
