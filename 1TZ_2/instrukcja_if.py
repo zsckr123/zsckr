d797ad5c295c857a99ddf0fc994be7c3b1d1ee47
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2024-01-11 11:58:12 +0100

    v11

--- 1TZ_2/instrukcja_if.py
+++ 1TZ_2/instrukcja_if.py
@@ -8,5 +8,7 @@ else:
 a = int(input('Podaj liczbe'))
 if a > 0:
     print('Liczba dodatnia')
+elif a <0:
+    print('Liczba ujemna')
 else:
-    print('Liczba niedodatnia')
+    print('liczba zero')
