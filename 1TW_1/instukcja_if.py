dc668efb861c0f5ec0bbd3e5026da60320623a73
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2024-01-29 13:34:55 +0100

    v12

---
+++ 1TW_1/instukcja_if.py
@@ -0,0 +1,14 @@
+'''
+if warunek:
+    instrukcja1
+else:
+    instrukcja2
+'''
+
+a = float(input('Podaj liczbe'))
+if a > 0:
+    print('Liczba dodatnia')
+elif a<0:
+    print('Liczba ujemna')
+else:
+    print('Liczba zero')
---
+++ 1TZ_1/instrukcja_for.py
@@ -0,0 +1,11 @@
+'''
+for i in range(10):   0-9
+    instrukcja
+'''
+
+for i in range(2,12,2):
+    print(i)
+
+
+for i in range(11,1,-1):
+    print(i)
