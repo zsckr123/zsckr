ea47a1e12a418d318765148a5f0f5534dd757fbc
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2024-01-30 09:58:03 +0100

    v13

---
+++ 1TR/funkcje_obliczeniowe_1.py
@@ -0,0 +1,15 @@
+'''
+
+f(x) = 2x-1
+'''
+# Funkcja z argumentem
+# def f_liniowa(x):
+#     return 2 * x - 1,x
+#
+# print(f' Wartosc funkcji dla argumentu {f_liniowa(2)[0]}:  {f_liniowa(2)[1]}')
+
+
+def f_liniowa(x):
+    return 2 * x - 1
+
+print(f' Wartosc funkcji:  {f_liniowa(2)}')
