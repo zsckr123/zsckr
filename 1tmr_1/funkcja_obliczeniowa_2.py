9e63c0f7620a51eb3666e62589bf8cc803514b30
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2024-02-05 14:21:04 +0100

    v16

---
+++ 1tmr_1/funkcja_obliczeniowa_2.py
@@ -0,0 +1,12 @@
+'''
+y = a x +b
+'''
+a = float(input('Podaj współczynnik a:'))
+b = float(input('Podaj współczynnik b:'))
+x = float(input('Podaj  x:'))
+def liniowa_1():
+    return a * x + b
+
+
+print(f' Wartość funkcji: {liniowa_1()} '
+      f'dla argumentu: {liniowa_1():}')
