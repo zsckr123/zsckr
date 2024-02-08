fb90505045850d2e0024482b0d66392f5345001e
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2024-02-08 13:52:05 +0100

    v17

---
+++ 1TZ_2/napisy_2.py
@@ -0,0 +1,13 @@
+
+a = input('Wprowadź tekst do zmiennej a')
+b = input('Wprowadź tekst do zmiennej b')
+# print(f' Drukowanie zmiennej a: {a} ')
+# print(f' Drukowanie zmiennej b: {b} ')
+''' Ile znaków ma tekst'''
+# print(len(a))
+
+''' Łączenie napisów '''
+c = a + b
+d = b + a
+print(c)
+print(d)
