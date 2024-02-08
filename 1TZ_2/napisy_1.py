baca4ff41819dd39f8bf8c0df2cc830458d22ac1
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2024-02-08 13:35:58 +0100

    v17

---
+++ 1TZ_2/napisy_1.py
@@ -0,0 +1,19 @@
+
+'''
+
+'dfdfdfd'
+''
+'''
+
+text = 'abcde'
+print(text)
+print(text[4]) # piąty element
+print(text[-1]) # ostatni element
+'''
+Drukowanie od początku do elementu o numerze 2 ale 
+bez tego elementu
+'''
+print(text[:2])
+# print('xxxxxxxxxxxxxxx   Drukowanie każdego elementu')
+for t in text:
+    print(t)
--- 1tmr_1/funkcje_obliczeniowe_3.py
+++ 1tmr_1/funkcje_obliczeniowe_3.py
@@ -4,6 +4,6 @@ b = float(input('Podaj współczynnik b:'))
 def liniowa_1(x):
     return  a * x + b
 
-for x in range(100):
+for x in range(100,0,-2):
     print(f' Wartość funkcji: {liniowa_1(x)} '
       f'dla argumentu: {x}')
