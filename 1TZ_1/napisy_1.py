3cfc86d3d0702a202fa71aa615fca81d01d50829
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2024-02-12 11:41:35 +0100

    v18

---
+++ 1TZ_1/napisy_1.py
@@ -0,0 +1,20 @@
+
+'''
+
+'dfdfdfd'
+''
+'''
+
+text = 'napis3'
+print(text)
+print(f'Piąty element ale o numerze 4 {text[4]}') # piąty element
+print(f'Ostatni element {text[-1]}') # ostatni element
+'''
+Drukowanie od początku do elementu o numerze/indeksie  2 ale 
+bez tego elementu
+'''
+print(text[:2])
+
+
+for t in text:
+    print(t)
