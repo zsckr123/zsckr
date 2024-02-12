bc2452c0ca6136b6c157243086c98c0cf24b6d77
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2024-02-12 14:34:29 +0100

    v20

---
+++ 1tmr_1/napis_2.py
@@ -0,0 +1,18 @@
+
+a = input('Wprowadź tekst do zmiennej a')
+b = input('Wprowadź tekst do zmiennej b')
+print(f' Drukowanie zmiennej a: {a} ')
+print(f' Drukowanie zmiennej b: {b} ')
+# # ''' Ile znaków ma tekst w zmiennej a'''
+print(len(a))
+# ''' CTRL + /'''
+#
+# ''' Łączenie napisów '''
+c = a + b
+d = b + a
+print(c)
+print(d)
+# #
+# # ''' Powielanie napisów'''
+print('Powielanie napisów')
+print(20 * 'x-')
