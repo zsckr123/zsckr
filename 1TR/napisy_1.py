9b50fea446acd2d202f252e512141c05d7ee3c32
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2024-02-13 09:57:08 +0100

    v21

---
+++ 1TR/napisy_1.py
@@ -0,0 +1,17 @@
+text = 'napis 2'
+# print(text)
+'''' CTRL + /'''
+# print(text[0]) # pierwszy element
+# print(text[-1]) # ostatni element
+# '''
+# Drukowanie od początku do elementu o indeksie 2 ale
+# bez tego elementu
+# '''
+print(text[:2])
+print(text[1:4])
+print(text[1:4:2])
+print(text[4:1:-2])
+print(text[-1::-1])
+# #
+for t in text:
+    print(t)
--- 1tmr_1/napis_3.py
+++ 1tmr_1/napis_3.py
@@ -1,7 +1,7 @@
 def dlugosc(tekst1, tekst2):
     if tekst1 > tekst2:
         return f'{tekst1} jest dłuższy {tekst2}'
-    elif  tekst2 > tekst1:
+    elif tekst2 > tekst1:
         return f'{tekst2} jest dłuższy {tekst1}'
     return f'napisy są jednakowej dlugości'
 
