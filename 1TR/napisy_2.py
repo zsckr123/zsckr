1d729e7684b6b1c3cf8b16b28ecf4ff130bb017a
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2024-02-13 10:10:40 +0100

    v21

---
+++ 1TR/napisy_2.py
@@ -0,0 +1,19 @@
+
+a = input('Wprowadź tekst do zmiennej a')
+b = input('Wprowadź tekst do zmiennej b')
+# print(f' Drukowanie zmiennej a: {a} ')
+# print(f' Drukowanie zmiennej b: {b} ')
+# # ''' Ile znaków ma tekst w zmiennej a'''
+print(len(a))
+print(len(b))
+# # ''' CTRL + /'''
+# #
+# # ''' Łączenie napisów '''
+c = a + b
+print(c)
+d = b + a
+print(d)
+# # #
+# # # ''' Powielanie napisów'''
+print('Powielanie napisów')
+print(20 * 'x-')
