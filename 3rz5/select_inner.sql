c46f494d558332e2ba887965f0e39d6a5366d953
Miroslaw Butajlo <miroslaw.butajlo@gmail.com>
2024-03-20 10:06:02 +0100

    project12

---
+++ 3rz5/select_inner.sql
@@ -0,0 +1,12 @@
+SELECT * FROM Employee e order by LastName DESC;
+
+SELECT LastName , Email  FROM Employee e WHERE LastName ='Adams';
+SELECT LastName , Email  FROM Employee  WHERE LastName LIKE 'p%'; 
+SELECT * FROM  Album a ;
+SELECT * FROM Album a WHERE Title LIKE 'b%';
+
+SELECT * FROM Employee e order by LastName DESC;
+SELECT * from Album a ;
+
+
+SELECT * FROM Album  INNER JOIN Artist  ON Album.ArtistId  = Artist.ArtistId  ;
