044b7d004fe3eb84f1614d765b0538b4308730ab
fedora <miroslaw.butajlo@estudzieniec.pl>
2024-03-27 13:00:58 +0100

    select_join

--- 3tw5_2/select.sql
+++ 3tw5_2/select.sql
@@ -1,3 +1,7 @@
 SELECT * FROM  Album ;
 SELECT Title  FROM  Album ;
 SELECT Title  FROM Album  WHERE Title LIKE 'b%';
+select * from Employee e ;
+select * from Artist a ;
+SELECT  *   from Album inner join Artist  on Album.ArtistId = Artist.ArtistId ;
+SELECT  Title , name  from Album inner join Artist  on Album.ArtistId = Artist.ArtistId ;
