3e0c1968d56283e0101413ab5f2ccd108b29da98
fedora <miroslaw.butajlo@estudzieniec.pl>
2024-04-03 13:00:55 +0200

    select_join

--- 3tw5_2/select.sql
+++ 3tw5_2/select.sql
@@ -9,3 +9,9 @@ select * from Track  ;
 SELECT Name, Title  from Track inner join Album on Track.AlbumId = Album.AlbumId ;
 
 select * from Track inner join MediaType on Track.MediaTypeId = MediaType.MediaTypeId ;
+
+select Track.Name,Track.Composer, MediaType.Name  from Track inner join MediaType on Track.MediaTypeId = MediaType.MediaTypeId ;
+
+SELECT * from Genre g ;
+select *  from Track inner join Genre on Track.GenreId  = Genre.GenreId  ;
+
