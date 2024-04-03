f7c26c09549fccb2880d3adfb4148b55fc4ba0fa
fedora <miroslaw.butajlo@estudzieniec.pl>
2024-04-03 12:51:40 +0200

    select_join

--- 3tw5_2/select.sql
+++ 3tw5_2/select.sql
@@ -7,3 +7,5 @@ SELECT  *   from Album inner join Artist  on Album.ArtistId = Artist.ArtistId ;
 SELECT  Title , name  from Album inner join Artist  on Album.ArtistId = Artist.ArtistId ;
 select * from Track  ;
 SELECT Name, Title  from Track inner join Album on Track.AlbumId = Album.AlbumId ;
+
+select * from Track inner join MediaType on Track.MediaTypeId = MediaType.MediaTypeId ;
